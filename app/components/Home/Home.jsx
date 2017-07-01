5afa116cff8b1ff029ad542edac78e7dbcb0282b
paulius.kiselis <paulius.kiselis@treatwell.com>
2017-07-02 02:11:02 +0300

    moved styles to sepparate file

--- app/components/Home/Home.jsx
+++ app/components/Home/Home.jsx
@@ -1,22 +1,12 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import styled from 'styled-components';
-
+import {
+  Title,
+  Header
+} from "./Styles";
 
 type Props        = { greeting: string, date: number };
 type State        = { greeting: string };
-
-const Title = styled.h1`
-	font-size: 1.5em;
-	text-align: center;
-	color: palevioletred;
-`;
-
-const Header = styled.h2`
-  font-size: 1em;
-  text-align: left;
-  color: hotpink;
-`
 
 
 export default class Main extends Component<void, Props, State> {
