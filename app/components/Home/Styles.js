4c86b536c05f05f603e81448b16c133d78af19d7
paulius.kiselis <paulius.kiselis@treatwell.com>
2017-08-25 23:41:03 +0300

    MB-1: fixed Home styles eslint rules.

--- app/components/Home/Styles.js
+++ app/components/Home/Styles.js
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 
 export const Title = styled.h1`
-	font-size: 1.5em;
-	text-align: center;
-	color: palevioletred;
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
 `;
 
 export const Header = styled.h2`
