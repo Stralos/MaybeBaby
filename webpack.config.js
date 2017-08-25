ae9d21ff40e59b208e2019e91d55eae32c32d026
paulius.kiselis <paulius.kiselis@treatwell.com>
2017-08-25 23:00:47 +0300

    MB-1: updated webpack version from 2 to 3. Added eslint-loader. Updated webpack config to use eslint loader.

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,49 @@
-"use strict";
-var path = require("path");
-var FlowStatusWebpackPlugin = require('flow-status-webpack-plugin');
+const path = require("path");
+const FlowStatusWebpackPlugin = require('flow-status-webpack-plugin');
 
-var config = {
-  devtool: "eval-source-map",
-  entry: path.join(__dirname, "/app/index.js"),
+const config = {
+  devtool: 'eval-source-map',
+  entry: path.join(__dirname, '/app/index.js'),
   output: {
-    path: path.join(__dirname, "/dist/"),
-    publicPath: path.join(__dirname, "/").replace(/\\/g, "/"),
-    filename: "bundle.js"
+    path: path.join(__dirname, '/dist/'),
+    publicPath: path.join(__dirname, '/').replace(/\\/g, '/'),
+    filename: 'bundle.js',
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx'],
   },
   module: {
-    loaders: [
+    rules: [
+      {
+        enforce: 'pre',
+        test: /.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+        options: {
+          failOnWarning: false,
+          failOnError: true,
+        },
+      },
       {
         test: /.jsx?$/,
-        loader: "babel-loader",
-        exclude: /node_modules/
-      }
-    ]
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
   },
   plugins: [
     new FlowStatusWebpackPlugin({
-      onError: function(stdout) {
-        console.log(stdout)
+      onError: (stdout) => {
+        console.log(stdout);
       },
-      onSuccess: function(stdout){
-        console.log(stdout)
+      onSuccess: (stdout) => {
+        console.log(stdout);
       },
       restartFlow: false,
       failOnError: true,
-      binaryPath: './node_modules/.bin/flow'
-    })
-  ]
-
+      binaryPath: './node_modules/.bin/flow',
+    }),
+  ],
 };
 
 module.exports = config;
