c901ee66b9a452fa98ddd6746e802757a0ab4165
paulius.kiselis <paulius.kiselis@treatwell.com>
2017-08-27 13:47:06 +0300

    MB-4: added simple tests

---
+++ app/components/Contacts/Contacts.snapshot.test.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import renderer from 'react-test-renderer';
+
+import Contacts from './Contacts';
+
+it('renders correctly', () => {
+  const tree = renderer.create(
+    <MemoryRouter>
+      <Contacts />
+    </MemoryRouter>,
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
