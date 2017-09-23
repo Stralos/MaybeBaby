66005f2790cfb55cac9162048c0d05fed8fbc84a
paulius.kiselis <paulius.kiselis@treatwell.com>
2017-09-23 22:22:50 +0300

    MB_6__refactored app.jsx

--- app/app.jsx
+++ app/app.jsx
@@ -3,40 +3,45 @@ import React from 'react';
 import styled from 'styled-components';
 import { withRouter, Route, NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
-import Home from './components/Home';
-import Contacts from './components/Contacts';
-import Services from './components/Services';
+import Nav from 'components/Nav';
+import SocialMediaIcon from 'components/SocialMediaIcon';
+import Home from './containers/Home';
 
 type Props = {
   +greeting: string,
   +date: number
 }
 
-const rando = 'active';
+const activeClassName = 'active';
 const StyledNavLink = styled(NavLink).attrs({
-  activeClassName: rando,
+  activeClassName,
 })`
-  color: palevioletred;
-  display: block;
-  margin: 0.5em 0;
-  font-family: Helvetica, Arial, sans-serif;
+  margin: 0 4px;
+  padding: 4px 8px;
+  color: #eee;
+  font-size: 16px;
+  text-decoration: none;
 
-  &.${rando} {
-    color: red;
+  &.${activeClassName} {
+    //font-weight: 600;
+  }
+  &:hover {
+    color: #c8c8c8;
   }
 `;
 
 const App = (props: Props) => {
   return (
     <div>
-      <nav>
+      <Nav>
         <StyledNavLink exact to="/"> Home </StyledNavLink>
-        <StyledNavLink exact to="/contacts"> Contacts </StyledNavLink>
-        <StyledNavLink exact to="/services"> Services </StyledNavLink>
-      </nav>
+        <StyledNavLink exact to="/a"> Service & Prices </StyledNavLink>
+        <StyledNavLink exact to="/v"> Our Team </StyledNavLink>
+        <StyledNavLink exact to="/b"> Gallery </StyledNavLink>
+      </Nav>
+      <SocialMediaIcon type={'blue'} />
+      <SocialMediaIcon type={'blue'} />
       <Route exact path={'/'} render={() => <Home {...props} />} />
-      <Route exact path={'/contacts'} component={Contacts} />
-      <Route exact path={'/services'} render={() => <Services />} />
     </div>
   );
 };
