c193823267d401a0fa94daf2d64f6d31b33dcf60
paulius.kiselis <paulius.kiselis@treatwell.com>
2017-09-23 22:23:06 +0300

    MB_6:added social media component.

---
+++ app/components/SocialMediaIcon/SocialMediaIcon.jsx
@@ -0,0 +1,18 @@
+/* @flow */
+import React from 'react';
+import styled from 'styled-components';
+
+type Props = {
+  +type: string
+}
+
+const SocialMediaIcon = styled.span`
+  height: 24px;
+  width: 24px;
+  display: inline-block;
+  background-color: ${(props: Props) => {
+    return props.type;
+  }}
+`;
+
+export default (props: Props) => <SocialMediaIcon {...props} />;
