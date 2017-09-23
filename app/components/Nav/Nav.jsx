5dabf5581a67603afdc0e43ce4ef8e64dc9fec9c
paulius.kiselis <paulius.kiselis@treatwell.com>
2017-09-23 22:23:18 +0300

    MB_6: added nav component.

---
+++ app/components/Nav/Nav.jsx
@@ -0,0 +1,13 @@
+/* @flow */
+import styled from 'styled-components';
+
+const Nav = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 0 -4px;
+  border-top: 1px solid #888;
+  border-bottom: 1px solid #888;
+  padding: 8px 0;
+`;
+
+export default Nav;
