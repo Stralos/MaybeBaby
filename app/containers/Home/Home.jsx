e53b1842d3c0cdea5a0dcf4d1966e543f4dd31df
paulius.kiselis <paulius.kiselis@wahanda.com>
2017-10-21 12:36:52 +0300

    MB_7: added more details to home page

--- app/containers/Home/Home.jsx
+++ app/containers/Home/Home.jsx
@@ -1,23 +1,95 @@
 /* @flow */
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import styled from 'styled-components';
 
 export type Props = {
   +description: string,
+  +products: Array<{|
+    +image: string,
+  |}>,
+  +workingHours: Array<{
+    +day: string,
+    +open: boolean,
+    +opensAt: string,
+    +closesAt: string,
+  }>,
 }
 
 export const Description = styled.div`
 
 `;
 
-export const Title = styled.h1`
+export const ProductList = styled.ul`
   font-size: 1.5em;
   text-align: center;
   color: palevioletred;
 `;
 
+export const Product = styled.li`
 
-export default class Home extends Component<Props> {
+`;
+
+export const Contacts = styled.div`
+  display: flex;
+`;
+export const ContactDetails = styled.div`
+
+`;
+export const Phone = styled.div`
+
+`;
+export const Address = styled.div`
+
+`;
+export const Email = styled.div`
+
+`;
+export const TimeTable = styled.ul`
+
+`;
+export const WorkTime = styled.ol`
+
+`;
+
+
+export default class Home extends PureComponent<Props> {
+  renderProductList = () => {
+    const { products } = this.props;
+    return (
+      <ProductList>
+        {products.map(product => <Product key={product.image} />)}
+      </ProductList>
+    );
+  }
+
+  renderTimeTable = () => {
+    const { workingHours } = this.props;
+    return (
+      <TimeTable>
+        { workingHours.map(time => <WorkTime key={time.day} />) }
+      </TimeTable>
+    );
+  }
+
+  renderContactDetails = () => {
+    return (
+      <ContactDetails>
+        <Address />
+        <Phone />
+        <Email />
+      </ContactDetails>
+    );
+  }
+
+  renderContacts = () => {
+    return (
+      <Contacts>
+        {this.renderContactDetails()}
+        {this.renderTimeTable()}
+      </Contacts>
+    );
+  }
+
   render() {
     const { description } = this.props;
     return (
@@ -25,6 +97,8 @@ export default class Home extends Component<Props> {
         <Description>
           { description }
         </Description>
+        {this.renderProductList()}
+        {this.renderContacts()}
       </div>
     );
   }
--- app/containers/Home/__tests__/Home.test.jsx
+++ app/containers/Home/__tests__/Home.test.jsx
@@ -2,11 +2,58 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import Home, { Description } from '../Home';
+import Home, {
+  Description,
+  ProductList,
+  Product,
+  TimeTable,
+  WorkTime,
+  ContactDetails,
+  Phone,
+  Email,
+  Address,
+} from '../Home';
 import type { Props } from '../Home';
 
 const props: Props = {
   description: 'Sarah Snips is a hairdressers and salon based in Marylebone, London.',
+  products: [{ image: 'a' }, { image: 'b' }],
+  workingHours: [{
+    day: 'Monday',
+    open: true,
+    opensAt: '08:00',
+    closesAt: '22:00',
+  }, {
+    day: 'Tuesday',
+    open: true,
+    opensAt: '08:00',
+    closesAt: '22:00',
+  }, {
+    day: 'Wendnesday',
+    open: true,
+    opensAt: '08:00',
+    closesAt: '22:00',
+  }, {
+    day: 'Thursday',
+    open: true,
+    opensAt: '08:00',
+    closesAt: '22:00',
+  }, {
+    day: 'Friday',
+    open: true,
+    opensAt: '08:00',
+    closesAt: '22:00',
+  }, {
+    day: 'Saturday',
+    open: true,
+    opensAt: '08:00',
+    closesAt: '22:00',
+  }, {
+    day: 'Sunday',
+    open: true,
+    opensAt: '08:00',
+    closesAt: '22:00',
+  }],
 };
 
 describe('Home Component -- ', () => {
@@ -16,5 +63,31 @@ describe('Home Component -- ', () => {
 
     expect(descriptionDOM).toHaveLength(1);
     expect(descriptionDOM.children().text()).toEqual(props.description);
+  });
+
+  it('should have a product list with 2 products', () => {
+    const wrapper = shallow(<Home {...props} />);
+    const productListDOM = wrapper.find(ProductList);
+
+    expect(productListDOM).toHaveLength(1);
+    expect(productListDOM.find(Product)).toHaveLength(2);
+  });
+
+  it('should have time table with work times', () => {
+    const wrapper = shallow(<Home {...props} />);
+    const timeTableDOM = wrapper.find(TimeTable);
+
+    expect(timeTableDOM).toHaveLength(1);
+    expect(timeTableDOM.find(WorkTime)).toHaveLength(7);
+  });
+
+  it('shoudl have contact details with email, phone and address', () => {
+    const wrapper = shallow(<Home {...props} />);
+    const contactDetailsDOM = wrapper.find(ContactDetails);
+
+    expect(contactDetailsDOM).toHaveLength(1);
+    expect(contactDetailsDOM.find(Email)).toHaveLength(1);
+    expect(contactDetailsDOM.find(Phone)).toHaveLength(1);
+    expect(contactDetailsDOM.find(Address)).toHaveLength(1);
   });
 });
