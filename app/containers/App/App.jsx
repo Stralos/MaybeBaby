5e3c8954d8a826498646d4450af81d7a0a4befba
paulius.kiselis <paulius.kiselis@wahanda.com>
2017-11-22 10:29:06 +0200

    MB__7: added routes. Added full height for app

--- app/containers/App/App.jsx
+++ app/containers/App/App.jsx
@@ -2,6 +2,7 @@
 import React, { PureComponent } from 'react';
 import styled, { injectGlobal } from 'styled-components';
 import { Route, NavLink, Switch } from 'react-router-dom';
+import ROUTES from 'utils/routes';
 import Nav from 'components/Nav';
 import SocialMediaIcon from 'components/SocialMediaIcon';
 import Services from 'components/Services';
@@ -17,6 +18,15 @@ export type Props = {
   },
 }
 
+const AppStyled = styled.div`
+  min-height: 100%;
+  margin: 0 auto;
+  max-width: 980px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
 const activeClassName = 'active';
 const StyledNavLink = styled(NavLink).attrs({
   activeClassName,
@@ -26,6 +36,9 @@ const StyledNavLink = styled(NavLink).attrs({
   color: #eee;
   font-size: 16px;
   text-decoration: none;
+  font-weight: 400;
+  letter-spacing: .1em;
+  text-transform: uppercase;
 
   &.${activeClassName} {
     //font-weight: 600;
@@ -44,10 +57,12 @@ const Header = styled.h1`
 `;
 
 const VenueName = styled(NavLink)`
+  font-family: 'Marcellus SC';
   font-size: 40px;
   line-height: 40px;
   color: #eee;
-  letter-spacing: 0.5em;
+  letter-spacing: 0.05em;
+  font-weight: 500;
   text-decoration: none;
   &:hover {
     color: #c8c8c8;
@@ -61,6 +76,13 @@ const SocialMedia = styled.div`
     margin: 0 12px;
   }
 `;
+const RouteContent = styled.div`
+  flex: 1;
+`;
+const Footer = styled.footer`
+  background: #222;
+  padding: 32px 0;
+`;
 
 class App extends PureComponent<Props> {
   componentWillMount() {
@@ -71,10 +93,12 @@ class App extends PureComponent<Props> {
     ** component should only display what he is given.
     */
     const globalStyles = `
-      body, html {
+      #root, body, html {
         margin: 0;
         height: 100%;
         min-height: 100%;
+      }
+      body, html {
         background-color: #333; 
         color: #ccc;
         font-family: 'Gentium Basic';
@@ -92,9 +116,9 @@ class App extends PureComponent<Props> {
       },
     } = this.props;
     return (
-      <div>
+      <AppStyled>
         <Header>
-          <VenueName to="/"> {venueName} </VenueName>
+          <VenueName to={ROUTES.HOME}> {venueName} </VenueName>
           <SocialMedia>
             <SocialMediaIcon href={facebook}> F </SocialMediaIcon>
             <SocialMediaIcon href={twitter}> T</SocialMediaIcon>
@@ -102,16 +126,22 @@ class App extends PureComponent<Props> {
           </SocialMedia>
         </Header>
         <Nav>
-          <StyledNavLink exact to="/"> Home </StyledNavLink>
-          <StyledNavLink exact to="/a"> Service & Prices </StyledNavLink>
-          <StyledNavLink exact to="/v"> Our Team </StyledNavLink>
-          <StyledNavLink exact to="/b"> Gallery </StyledNavLink>
+          <StyledNavLink exact to={ROUTES.HOME}> Home </StyledNavLink>
+          <StyledNavLink exact to={ROUTES.SERVICES}> Service & Prices </StyledNavLink>
+          <StyledNavLink exact to={ROUTES.TEAM}> Our Team </StyledNavLink>
+          <StyledNavLink exact to={ROUTES.GALLERY}> Gallery </StyledNavLink>
+          <StyledNavLink exact to={ROUTES.CONTACT}> Contact </StyledNavLink>
         </Nav>
-        <Switch>
-          <Route exact path={'/'} component={Home} />
-          <Route exact path={'/a'} component={Services} />
-        </Switch>
-      </div>
+        <RouteContent>
+          <Switch>
+            <Route exact path={ROUTES.HOME} component={Home} />
+            <Route exact path={ROUTES.SERVICES} component={Services} />
+          </Switch>
+        </RouteContent>
+        <Footer>
+          Footer
+        </Footer>
+      </AppStyled>
     );
   }
 }
