39a4ac02952f9c55df694c05f77d35d25f1b987e
paulius.kiselis <paulius.kiselis@wahanda.com>
2017-12-02 22:49:23 +0200

    added types for jest

--- flow-typed/myTypes/jest.js
+++ flow-typed/myTypes/jest.js
@@ -1,5 +1,3 @@
 /* @flow */
-declare function describe(t: string, f: any): void;
-declare function it(t: string, f: any): void;
-declare function expect(t: any): any;
-declare function beforeEach(fn: Function, timeOut?: number): void;
+/* eslint no-undef: 0 */
+
