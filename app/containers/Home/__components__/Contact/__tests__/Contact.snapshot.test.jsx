5c3b7af2d1eec6c2421cbae4874fdf6a205bc2e6
paulius.kiselis <paulius.kiselis@wahanda.com>
2017-12-06 16:52:00 +0200

    added snapshot tests for Contact component.

---
+++ app/containers/Home/__components__/Contact/__tests__/Contact.snapshot.test.jsx
@@ -0,0 +1,24 @@
+/* @flow */
+import React from 'react';
+import renderer from 'react-test-renderer';
+import 'jest-styled-components';
+
+import Contact from '../Contact';
+import type { Props } from '../Contact';
+
+const getProps = (): Props => {
+  return {
+    lable: '',
+    contactInformation: '',  
+  };
+};
+
+describe('Contact Component Snapshot', () => {
+  it('renders correctly', () => {
+    const props = getProps();
+    const tree = renderer.create(
+      <Contact {...props} />,
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
